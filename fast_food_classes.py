87da9eb4f99cdd84556509beaa806f9d1371218e
Joeladmin <joel.jancarik@gmail.com>
2017-12-10 17:00:24 +0100

    Classes created and cook class expanded

---
+++ fast_food_classes.py
@@ -0,0 +1,65 @@
+class customer:
+#    int id_customer
+#    int start_time
+#    int patience
+#    bool take_away
+#    int time_to_eat
+    def _init_(self,id_customer,start_time,patience,take_away,time_to_eat):
+        self.id_customer=id_customer
+        self.start_time=start_time
+        self.patience=patience
+        self.take_away=take_away
+        self.time_to_eat=time_to_eat
+
+class service:
+#    int id_service
+#    int time_to_order
+#    int time_to_prepare
+    def _init_(self,id_service,time_to_order,time_to_prepare):
+        self.id_service=id_service
+        self.time_to_order=time_to_order
+        self.time_to_prepare=time_to_prepare
+        self.queue=[]
+    def enqueue(self,customer):
+        self.queue.append(customer)
+    def dequeue(self):
+        self.queue.pop(0)
+    def length_queue(self):
+        return(len(self.queue))
+
+class cook:
+#    int id_cook
+#    int time_to_cook
+    def _init_(self,id_cook,time_to_cook):
+        self.id_cook=id_cook
+        self.time_to_cook=time_to_cook
+        queue=[]
+        preparation_time=0
+    def enqueue(self,service):
+        if queue==[]:
+            preparation_time=0
+        self.queue.append(sevice)
+    def is_cooked():
+        if self.queue<>[] and self.preparation_time % self.time_to_cook==0:
+            self.queue.pop(0)
+            preparation_time=0
+
+class fast_food_model:
+#   float unit
+#   hh:mm:ss open and close time
+#   int places
+#   list of cooks
+#   list of service
+#   list of customers (ordered by start_time)
+    def _init_():
+        pass
+    def read_empolyes():
+        pass
+    def new_customer():
+        pass
+    def serve_food():
+        pass
+    def find_best_cook():
+        pass
+    def find_shortest_queue():
+        pass
